1827ccc467dee8e2cce15c2d70319016e07e73c1
Aurelien <aureliendumont@MacBook-Pro.local>
2014-02-14 10:47:12 +0100

    UI

--- web/bundles/hcocktail/js/scripts.js
+++ web/bundles/hcocktail/js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	 $('.selectpicker').selectpicker();
+	 var s = Snap("#glace");
+	 var bigCircle = s.polyline(10, 10, 100, 100);
+	//  bigCircle.attr({
+	//     fill: "#bada55",
+	//     stroke: "#000",
+	//     strokeWidth: 5
+	// });
+	//  var smallCircle = s.circle(100, 150, 70);
+	// var discs = s.group(smallCircle, s.circle(200, 150, 70));
 });
